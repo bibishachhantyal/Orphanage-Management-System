4928c5651f39371f88a27cc12db5cf44e86ef310
Rakshya Phagami <phagamirakshya@gmail.com>
2026-04-18 11:03:21 +0545

    Implement connection pooling with HikariCP and add SLF4J logging

--- src/main/java/com/orphanage/utils/DatabaseConnection.java
+++ src/main/java/com/orphanage/utils/DatabaseConnection.java
@@ -1,24 +1,24 @@
 package com.orphanage.utils;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/orphanage_db";
-    private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "";
+    private static HikariDataSource dataSource;
 
     static {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/orphanage_db");
+        config.setUsername("root");
+        config.setPassword("");
+        config.setMaximumPoolSize(10);
+        dataSource = new HikariDataSource(config);
     }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        return dataSource.getConnection();
     }
 
     public static void closeConnection(Connection conn) {
---
+++ src/main/java/com/orphanage/utils/TestConnection.java
@@ -0,0 +1,4 @@
+package com.orphanage.utils;
+
+public class TestConnection {
+}
